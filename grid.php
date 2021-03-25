d96f4813d7da6cc639b07484e4360b19c78e76d6
acs <a.bederiat@codeur.online>
2021-03-25 16:02:05 +0100

    test de la grille de bootstrap

---
+++ grid.php
@@ -0,0 +1,70 @@
+<?php include "header.php" ; ?>
+
+<div class="container-fluide">
+    <div class="row">
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12">karina</div>
+        <div class="col-md-4 col-sm-12">Sonia</div>
+        <div class="col-md-4 col-sm-12">Romain</div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-sm-12">Etienne</div>
+        <div class="col-md-8 col-sm-12">Adam</div>
+        <div class="col-md-2 col-sm-12">Adrien</div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12">karina</div>
+        <div class="col-md-4 col-sm-12">Sonia</div>
+        <div class="col-md-4 col-sm-12">Romain</div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12">Etienne</div>
+        <div class="col-md-8 col-sm-12">
+        <div class="row">
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+        <div class="col-md-1 col-sm-12 border-ad">test</div>
+    </div>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ; ?>
